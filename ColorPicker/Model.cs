81d05398ab1f1f9a6965b1a5989ce9b7eaa3c844
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2024-01-12 01:21:32 +0200

    done

---
+++ ColorPicker/Model.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace ColorPicker
+{
+    class MyColor : DependencyObject
+    {
+        private static readonly DependencyProperty AlphaProperty;
+
+        private static readonly DependencyProperty RedProperty;
+
+        private static readonly DependencyProperty GreenProperty;
+
+        private static readonly DependencyProperty BlueProperty;
+
+        private static readonly DependencyProperty ColorARGB_Property;
+
+
+
+        public int Alpha
+        {
+            get { return (int)GetValue(AlphaProperty); }
+            set { SetValue(AlphaProperty, value); }
+        }
+
+        public int Red
+        {
+            get { return (int)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+
+        public int Green
+        {
+            get { return (int)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public int Blue
+        {
+            get { return (int)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        public SolidColorBrush Col
+        {
+            get { return (SolidColorBrush)GetValue(ColorARGB_Property); }
+            set { SetValue(ColorARGB_Property, value); }
+        }
+
+        public MyColor(int alpha, int red, int green, int blue, SolidColorBrush col)
+        {
+            Alpha = alpha;
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Col = col;
+        }
+
+        static MyColor()
+        {
+            AlphaProperty = DependencyProperty.Register("Alpha", typeof(int), typeof(MyColor));
+            RedProperty = DependencyProperty.Register("Red", typeof(int), typeof(MyColor));
+            GreenProperty = DependencyProperty.Register("Green", typeof(int), typeof(MyColor));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(int), typeof(MyColor));
+            ColorARGB_Property = DependencyProperty.Register("Col", typeof(SolidColorBrush), typeof(MyColor));
+        }
+
+    }
+}
---
+++ ColorPicker/ViewModel.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Drawing;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+using System.ComponentModel;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using ColorPicker;
+
+namespace ColorPicker
+{
+    class ARGB_ViewModel : INotifyPropertyChanged
+    {
+
+        public MyCommands addComand;
+        public MyCommands delComand;
+
+        public ICommand AddCommand
+        {
+            get { return addComand; }
+        }
+
+
+        public ICommand DelCommand
+        {
+            get { return delComand; }
+        }
+
+        private int _SelectedIndex;
+        public int SelectedIndex
+        {
+            get { return _SelectedIndex; }
+            set
+            {
+                if (!value.Equals(_SelectedIndex))
+                {
+                    _SelectedIndex = value;
+                    OnPropertyChanged("SelectedIndex");
+                }
+            }
+        }
+
+
+
+        private bool _Check1;
+        public bool Check1
+        {
+            get { return _Check1; }
+            set
+            {
+                if (!value.Equals(_Check1))
+                {
+                    _Check1 = value;
+                    Alpha = 0;
+                    OnPropertyChanged("Check1");
+
+                }
+            }
+        }
+
+        private bool _Check2;
+        public bool Check2
+        {
+            get { return _Check2; }
+            set
+            {
+                if (!value.Equals(_Check2))
+                {
+                    _Check2 = value;
+                    Red = 0;
+                    OnPropertyChanged("Check2");
+
+                }
+            }
+        }
+
+        private bool _Check3;
+        public bool Check3
+        {
+            get { return _Check3; }
+            set
+            {
+                if (!value.Equals(_Check3))
+                {
+                    _Check3 = value;
+                    Green = 0;
+                    OnPropertyChanged("Check3");
+
+                }
+            }
+        }
+        private bool _Check4;
+        public bool Check4
+        {
+            get { return _Check4; }
+            set
+            {
+                if (!value.Equals(_Check4))
+                {
+                    _Check4 = value;
+                    Blue = 0;
+                    OnPropertyChanged("Check4");
+
+                }
+            }
+        }
+        public ObservableCollection<MyColor> MyColor { get; set; } = new ObservableCollection<MyColor>();
+
+
+        private SolidColorBrush _colorARGB;
+        public SolidColorBrush ColorARGB
+        {
+            get { return _colorARGB; }
+            set
+            {
+                if (!value.Equals(_colorARGB))
+                {
+                    _colorARGB = value;
+                    OnPropertyChanged("ColorARGB");
+                }
+            }
+        }
+
+        private int _alpha;
+        public int Alpha
+        {
+            get { return _alpha; }
+            set
+            {
+                if (_alpha != value)
+                {
+                    _alpha = value;
+                    OnPropertyChanged("Alpha");
+                    UpdateButtonColor();
+                }
+            }
+        }
+
+        private int _red;
+        public int Red
+        {
+            get { return _red; }
+            set
+            {
+                if (_red != value)
+                {
+                    _red = value;
+                    OnPropertyChanged("Red");
+                    UpdateButtonColor();
+                }
+            }
+        }
+
+        private int _green;
+        public int Green
+        {
+            get { return _green; }
+            set
+            {
+                if (_green != value)
+                {
+                    _green = value;
+                    OnPropertyChanged("Green");
+                    UpdateButtonColor();
+                }
+            }
+        }
+
+        private int _blue;
+        public int Blue
+        {
+            get { return _blue; }
+            set
+            {
+                if (_blue != value)
+                {
+                    _blue = value;
+                    OnPropertyChanged("Blue");
+                    UpdateButtonColor();
+                }
+            }
+        }
+
+        public ARGB_ViewModel()
+        {
+            UpdateButtonColor();
+
+            addComand = new MyCommands(param => Add(), param => CanAdd());
+            delComand = new MyCommands(param => Del(), param => CanDel());
+
+        }
+
+        public void Add()
+        {
+
+            MyColor cl = new MyColor(Alpha, Red, Green, Blue, ColorARGB);
+
+            MyColor.Add(cl);
+
+
+
+
+        }
+
+        public bool CanAdd()
+        {
+            foreach (var item in MyColor)
+            {
+                if (item.Alpha == Alpha && item.Green == Green && item.Red == Red && item.Blue == Blue)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void Del()
+        {
+            MessageBoxResult res = MessageBox.Show("Delete color?", "Delete color",
+            MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.Yes) MyColor.RemoveAt(SelectedIndex);
+        }
+
+        public bool CanDel()
+        {
+            if (MyColor.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void UpdateButtonColor()
+        {
+            ColorARGB = new SolidColorBrush(Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
